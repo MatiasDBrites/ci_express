cf61312b97909d7a6b2ac1784c435398b62aabd8
Matias D Brites <104856701+MatiasDBrites@users.noreply.github.com>
2022-10-27 13:41:28 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,24 @@
 import { Sequelize } from "sequelize";
-import * as dotenv from 'dotenv';
+
 import _config from "../config/config.js";
+
 const env = process.env.ENV;
 const config = _config[env];
-dotenv.config();
 
+// create connection
 const db = new Sequelize(
-process.env.DB_NAME,
-process.env.DB_USER,
-process.env.DB_PASSWORD,
-{
-host: process.env.DB_HOST,
-dialect: "postgres",
-logging: false,
-port: process.env.DB_PORT,
-define: {
-timestamps: false 
-}
-});
-export default db; 
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: 'postgres',
+    logging: config.logging,
+    define: {
+      timestamps: false
+    }
+  },
+  
+);
+
+export default db;
